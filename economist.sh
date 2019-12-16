ccf879219e37241841f4a878b31e5c2bba058d18
toyqiu <toyqiuwu@gmail.com>
2019-12-17 02:16:34 +0800

    Update economist.sh

--- economist.sh
+++ economist.sh
@@ -1,5 +1,5 @@
 # recipe文件路径
-re_path=/home/username/calibre/recipes # 请修改为自己系统中的对应文件夹
+re_path=/books/recipes # 请修改为自己系统中的对应文件夹
 
 cd $re_path
 for name in `ls Economist.recipe|awk -F '.' '{print $1}'`
@@ -8,7 +8,7 @@ do
         ebook-convert ${name}.recipe .epub
         sleep 3
         echo -e "\n       将生成的杂志 $name 添加到书库 ... \n"
-        calibredb add --with-library /home/username/calibre/ ${name}.epub # 请修改为自己系统中的对应文件夹
+        calibredb add --with-library /books/ ${name}.epub # 请修改为自己系统中的对应文件夹
 		sleep 3
         echo -e "\n       将删除已添加到书库的杂志 ... \n"
 		rm -rf ${name}.epub
